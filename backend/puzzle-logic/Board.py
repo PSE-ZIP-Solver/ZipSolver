1fb39c9fddd266667f004ca422072ad8e6c7e3a3
FJ31415 <fj-otg@outlook.de>
2026-06-30 23:45:43 +0200

    Fixed imports

--- backend/puzzle-logic/Board.py
+++ backend/puzzle-logic/Board.py
@@ -1,12 +1,13 @@
-import Position
-import Waypoint
-import Wall
+from Position import Position
+from Waypoint import Waypoint
+from Wall import Wall
+from typing import List, Set, Optional
 
 class Board:
     def __init__(self, size: int):
         self._size = size
-        self._waypoints = list()
-        self._walls = set()
+        self._waypoints: List[Waypoint] = []
+        self._walls: Set[Wall] = set()
 
     def addWaypoint(self, position: Position, order: int):
         waypoint = Waypoint(position, order)
@@ -17,10 +18,10 @@ class Board:
         self._walls.add(wall)
 
     def isInside(self, position: Position) -> bool:
-        return 0 <= position.getX() < self._size and 0 <= position.getY() < self._size
+        return 0 <= position.getX < self._size and 0 <= position.getY < self._size
 
     def areAdjacent(self, a: Position, b: Position) -> bool:
-        return abs(a.x - b.x) + abs(a.y - b.y) == 1
+        return abs(a.getX - b.getX) + abs(a.getY - b.getY) == 1
 
     def hasWallBetween(self, a: Position, b: Position) -> bool:
         for wall in self._walls:
@@ -28,19 +29,19 @@ class Board:
                 return True
         return False
 
-    def getWaypointAt(self, position: Position)-> Waypoint:
+    def getWaypointAt(self, position: Position) -> Optional[Waypoint]:
         for wp in self._waypoints:
-            if wp.position.equals(position):
+            if wp.getPosition == position:
                 return wp
         return None
     
-    def getWaypointByOrder(self, order: int) -> Waypoint:
+    def getWaypointByOrder(self, order: int) -> Optional[Waypoint]:
         for wp in self._waypoints:
-            if wp.order == order:
+            if wp.getOrder == order:
                 return wp
         return None
 
-    def getAllPositions(self):
+    def getAllPositions(self) -> Set[Position]:
         positions = set()
         for y in range(self._size):
             for x in range(self._size):
@@ -51,13 +52,13 @@ class Board:
         return self._size * self._size
     
     @property
-    def getSize(self):
+    def getSize(self) -> int:
         return self._size
     
     @property
-    def getWaypoints(self):
+    def getWaypoints(self) -> List[Waypoint]:
         return self._waypoints
     
     @property
-    def getWalls(self):
+    def getWalls(self) -> Set[Wall]:
         return self._walls
