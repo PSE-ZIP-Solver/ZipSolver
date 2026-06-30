fbf772055622299d8716e19ce913a06e4d964207
FJ31415 <fj-otg@outlook.de>
2026-06-30 15:16:41 +0200

    Added Getters and Setters to Position, Wall & Waypoint

--- backend/puzzle-logic/Wall.py
+++ backend/puzzle-logic/Wall.py
@@ -2,11 +2,19 @@ import Position
 
 class Wall:
     def __init__(self, a: Position, b: Position):
-        self.cellA = a
-        self.cellB = b
+        self._cellA = a
+        self._cellB = b
 
+    @property
+    def getCellA(self) -> Position:
+        return self._cellA
+
+    @property
+    def getCellB(self) -> Position:
+        return self._cellB
+    
     def connects(self, a: Position, b: Position) -> bool:
-        return (self.cellA == a and self.cellB == b) or (self.cellA == b and self.cellB == a)
+        return (self._cellA == a and self._cellB == b) or (self._cellA == b and self._cellB == a)
     
     def __eq__(self, other):
-        return  isinstance(other, Wall) and self.connects(other.cellA, other.cellB)
+        return  isinstance(other, Wall) and self.connects(other._cellA, other._cellB)
