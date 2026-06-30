4070c0a085f2ac793d5214b0c232e2ed98f5f940
FJ31415 <fj-otg@outlook.de>
2026-06-30 14:45:54 +0200

    Standardized Waypoint

--- backend/puzzle-logic/Waypoint.py
+++ backend/puzzle-logic/Waypoint.py
@@ -2,5 +2,5 @@ import Position
 
 class Waypoint:
     def __init__(self, position: Position, order: int):
-        self._position = position
-        self._order = order
+        self.position = position
+        self.order = order
