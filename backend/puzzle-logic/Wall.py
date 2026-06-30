5f5d2a5832997cabcade8bfe00adb03115794061
FJ31415 <fj-otg@outlook.de>
2026-06-30 14:53:51 +0200

    Added return-type to Wall.connects(a, b)

--- backend/puzzle-logic/Wall.py
+++ backend/puzzle-logic/Wall.py
@@ -5,7 +5,7 @@ class Wall:
         self.cellA = a
         self.cellB = b
 
-    def connects(self, a: Position, b: Position):
+    def connects(self, a: Position, b: Position) -> bool:
         return (self.cellA == a and self.cellB == b) or (self.cellA == b and self.cellB == a)
     
     def __eq__(self, other):
