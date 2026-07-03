eab6bc6edd2c97b7d49286f5895091a53fa2b1a5
FJ31415 <fj-otg@outlook.de>
2026-07-03 12:08:25 +0200

    Added intis

--- offline_training/tests/unit/TestBoardGenerator.py
+++ offline_training/tests/unit/TestBoardGenerator.py
@@ -1,6 +1,6 @@
 import unittest
-from offline_training.BoardGenerator import BoardGenerator
 from backend.PuzzleLogic import Board, Position
+from offline_training.BoardGenerator import BoardGenerator
 
 class TestBoardGenerator(unittest.TestCase):
 
