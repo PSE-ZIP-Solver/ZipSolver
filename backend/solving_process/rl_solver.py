f722846e4f40e70b76276327f1cf637d06344c2c
FJ31415 <109368000+FJ31415@users.noreply.github.com>
2026-07-08 16:04:23 +0200

    small fix

--- backend/solving_process/rl_solver.py
+++ backend/solving_process/rl_solver.py
@@ -1,8 +1,5 @@
-from typing import Optional
-
-# Adjust imports based on your project structure
-from .solver import Solver
-from .solver_result import SolverResult
+from solver import Solver
+from solver_result import SolverResult
 from backend.puzzle_logic.board import Board
 from backend.rl_components.RLAgent import RLAgent
 from backend.rl_components.RLEnvironment import RLEnvironment
@@ -10,18 +7,10 @@ from backend.rl_components.RLEnvironment import RLEnvironment
 
 class RLSolver(Solver):
     def __init__(self):
-        self._agent: Optional[RLAgent] = None
-        self._environment: Optional[RLEnvironment] = None
+        self._agent: RLAgent = None # TODO implement
+        self._environment: RLEnvironment = None # TODO implement
 
-    @property
-    def getAgent(self) -> Optional[RLAgent]:
-        return self._agent
-
-    @property
-    def getEnvironment(self) -> Optional[RLEnvironment]:
-        return self._environment
-
-    def loadAgent(self) -> RLAgent:
+    def _loadAgent(self) -> RLAgent:
         """
         Loads the pre-trained RL agent model.
 
@@ -31,7 +20,7 @@ class RLSolver(Solver):
         # TODO implement
         pass
 
-    def runEpisode(self, board: Board) -> SolverResult:
+    def _runEpisode(self, board: Board) -> SolverResult:
         """
         Runs one RL episode for the given board and returns the resulting solver result.
 
