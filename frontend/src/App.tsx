2c69eca8e2d9c2f1da909b30b581c6a87b0eed82
udlru <udlru@student.kit.edu>
2026-07-08 17:02:57 +0200

    feat: add Navbar component with logo and navigation buttons; update App component to include Navbar; index.css file now have a global types

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,12 @@
 import { useState } from 'react';
-//import Navbar from './components/Navbar';
+import Navbar from './components/Navbar';
 //import Footer from './components/Footer';
 //import GridBuilder from './components/GridBuilder';
 
 export const App: React.FC = () => {
   return (
     <div className="min-h-screen flex flex-col bg-gray-50 text-gray-900">
-      {/*<Navbar />*/}
+      <Navbar />
       
       <main className="flex-1 w-full pt-2">
         {/*<GridBuilder />*/}
