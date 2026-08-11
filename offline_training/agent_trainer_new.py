41723267dbf0da7a712ade24e5d2d7c3a6444c10
Vincent <90052776+Vincent-hdgt@users.noreply.github.com>
2026-08-11 22:07:18 +0200

    Setup for 7. run training

--- offline_training/agent_trainer_new.py
+++ offline_training/agent_trainer_new.py
@@ -315,7 +315,7 @@ class AgentTrainer:
 
             # Lower exploration for fine-tuning an already trained model.
             agent.set_exploration_schedule(
-                initial_eps=0.35,
+                initial_eps=0.6,
                 final_eps=0.1,
                 fraction=0.8,
             )
@@ -325,7 +325,7 @@ class AgentTrainer:
             agent = RLAgent(
                 trainEnv,
                 learning_rate=1e-4,              # Controls how strongly the network weights are changed during each gradient/network update.
-                exploration_initial_eps=0.35,     # Initial probability of choosing a random action.
+                exploration_initial_eps=0.6,     # Initial probability of choosing a random action.
                 exploration_final_eps=0.1,      # Final minimum probability of choosing a random action.
                 exploration_fraction=0.8,        # Fraction of training over which exploration is reduced.
                 learning_starts=2000,             # Number of steps before the model starts learning.
@@ -517,17 +517,17 @@ class AgentTrainer:
 
 if __name__ == "__main__":
     RANDOMIZE_BOARD_COMPLEXITY = True
-    MIN_NR_OF_WALLS = 44
+    MIN_NR_OF_WALLS = 30
     NR_OF_WALLS = 45
-    MIN_NR_OF_WAYPOINTS = 44
+    MIN_NR_OF_WAYPOINTS = 30
     NR_OF_WAYPOINTS = 45
 
     USE_SAVED_TRAINING_BOARDS = True
     LOAD_REPLAY_BUFFER = True # only True for several runs on same training set (continue session)
-    TRAINING_BOARDS_PATH = ("offline_training/training_boards/06-8x8.pkl")
+    TRAINING_BOARDS_PATH = ("offline_training/training_boards/07-8x8.pkl")
 
     USE_SAVED_EVALUATION_BOARDS = False
-    EVALUATION_BOARDS_PATH = "offline_training/evaluation_boards/06-8x8.pkl"
+    EVALUATION_BOARDS_PATH = "offline_training/evaluation_boards/07-8x8.pkl"
 
     TRAIN_MODEL = True
     PRINT_TRAINING_BOARDS = True
@@ -540,18 +540,18 @@ if __name__ == "__main__":
         boardSize=8,
         nrOfWalls=NR_OF_WALLS,
         nrOfWaypoints=NR_OF_WAYPOINTS,
-        modelPath="offline_training/trained_models/06-8x8.zip",
+        modelPath="offline_training/trained_models/07-8x8.zip",
         minNrOfWalls=MIN_NR_OF_WALLS,
         minNrOfWaypoints=MIN_NR_OF_WAYPOINTS,
 
         # number of training boards in the training pool
-        nrTrainingBoards=30,
+        nrTrainingBoards=60,
 
         # Evaluation boards for testing the saved model.
         nrEvaluationBoards=100,
 
         # Total time steps
-        timestepsPerBoard=1_000_000,
+        timestepsPerBoard=1_500_000,
 
         loadExistingModel=True,
         resetModel=False,
